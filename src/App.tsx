a09bf1610d58adfcb34332249b641227cb2ed0c6
teledildonics-dev <57303078+teledildonics-dev@users.noreply.github.com>
2019-11-06 01:01:46 -0500

    view source

--- src/App.tsx
+++ src/App.tsx
@@ -317,18 +317,23 @@ const App: FC = () => {
           border: "1px solid black",
           color: "#000",
           background: "#FFF8F0",
-          fontSize: "14px",
+          fontSize: "12px",
           fontFamily: "sans-serif",
-          padding: "8px",
+          padding: "10px",
           borderRadius: "4px",
           display: "inline-block",
           margin: "4px",
-          float: "right"
+          float: "right",
+          lineHeight: "18px"
         }}
       >
         teledildonics.dev: my control panel of pleasure. <br />
-        Buggy and unstable. Only supports some{" "}
-        <a href="https://www.lovense.com/compare">Lovense</a> devices.
+        Buggy and unstable.{" "}
+        <a href="https://github.com/teledildonics-dev/teledildonics-dev">
+          View source here
+        </a>
+        .<br />
+        Only supports some <a href="https://www.lovense.com/compare">Lovense</a> devices.
       </div>
       <DevicePanes />
     </main>
