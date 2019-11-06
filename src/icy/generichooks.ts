0183f8449942d86f23156e92fb1f261fb7277c8a
teledildonics-dev <57303078+teledildonics-dev@users.noreply.github.com>
2019-11-05 20:32:47 -0500

    useSharedState was just a worse useRef

--- src/icy/generichooks.ts
+++ src/icy/generichooks.ts
@@ -1,34 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { sleep } from "./async";
-
-export const useSharedState = <T>(
-  initialValue: T
-): null | [() => T, (value: T) => void] => {
-  // As if this weren't complicated enough, we need to wrap each of our
-  // functions as a single-element array because if you pass a function
-  // into useState()'s setters directly, it will be called to transform
-  // the stored value, but we want to store the function itself.
-  const [getter, setGetter] = useState<([(() => T)]) | null>(null);
-  const [setter, setSetter] = useState<([((value: T) => void)]) | null>(null);
-
-  useEffect(() => {
-    let value = initialValue;
-
-    setGetter([() => value]);
-    setSetter([
-      (newValue: T) => {
-        value = newValue;
-      }
-    ]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (getter && setter) {
-    return [getter[0], setter[0]];
-  } else {
-    return null;
-  }
-};
 
 export const useThrottledChanges = <T extends unknown>(interval: number, value: T) => {
   /// Whether this value is currently throttled, meaning that the value has
@@ -36,44 +7,38 @@ export const useThrottledChanges = <T extends unknown>(interval: number, value: 
   /// an async function, which will set throttled to false once the interval has
   /// elapsed, and check if the target value has changed and needs to be updated,
   /// which would reset the throttle.
-  const throttledAccessors = useSharedState(false);
+  const throttled = useRef<boolean>(false);
 
   /// The throttled value we are outputting.
   const [throttledValue, setThrottledValue] = useState<T>(value);
 
   /// A shared state storing the latest input value, which may not yet be
   /// reflected to the output throttledValue if we're throttled.
-  const targetValueAccessors = useSharedState<T>(value);
+  const targetValue = useRef<T>(value);
 
   useEffect(() => {
-    if (!(targetValueAccessors && throttledAccessors)) {
-      return;
-    }
-    const [getTargetValue, setTargetValue] = targetValueAccessors;
-    const [getThrottled, setThrottled] = throttledAccessors;
+    targetValue.current = value;
 
-    setTargetValue(value);
-
-    if (!getThrottled()) {
-      setThrottled(true);
+    if (!throttled.current) {
+      throttled.current = true;
       setThrottledValue(value);
 
       const checkAsync = async (initialValue: T) => {
         await sleep(interval);
 
-        const latestTargetValue = getTargetValue();
+        const latestTargetValue = targetValue.current;
         if (initialValue !== latestTargetValue) {
           setThrottledValue(latestTargetValue);
           checkAsync(latestTargetValue);
         } else {
-          setThrottled(false);
+          throttled.current = false;
         }
       };
 
       checkAsync(value);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value, targetValueAccessors, throttledAccessors]);
+  }, [value]);
 
   return throttledValue;
 };
